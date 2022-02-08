f0891a9820be87762e38ff7b90b19b96a1e72379
caojiaqi8 <caojiaqi8@jd.com>
2022-02-08 10:11:02 +0800

    博客前端代码

---
+++ src/components/Home/Work.vue
@@ -0,0 +1,162 @@
+<template>
+  <article class="work">
+    <h2>WORK EXPERIENCE</h2>
+    <ul>
+      <li>
+        <div class="left">
+          <p class="time">2020-</p>
+        </div>
+        <div class="right">
+          <h3>WEB DEVELOPER/ONLINE TEACHER</h3>
+          <p class="img">
+            <img src="../../assets/img/technology/vue.jpg" width="auto" height="30" alt="">
+          </p>
+          <p class="company">Zzt EDU</p>
+          <p class="content">
+            Responsible for the development of independent official website of some subjects of the company.And also as an online teacher of WEB.Technology consists of HTML5, CSS3, JavaScript, Nodejs, vue, Canvas, Design pattern & Algorithm。
+          </p>
+        </div>
+      </li>
+      <li>
+        <div class="left">
+          <p class="time">2020-</p>
+        </div>
+        <div class="right">
+          <h3>WEB DEVELOPER/ONLINE TEACHER</h3>
+          <p class="img">
+            <img src="../../assets/img/technology/vue.jpg" width="auto" height="30" alt="">
+          </p>
+          <p class="company">Zzt EDU</p>
+          <p class="content">
+            Responsible for the development of independent official website of some subjects of the company.And also as an online teacher of WEB.Technology consists of HTML5, CSS3, JavaScript, Nodejs, vue, Canvas, Design pattern & Algorithm。
+          </p>
+        </div>
+      </li>
+      <li>
+        <div class="left">
+          <p class="time">2020-</p>
+        </div>
+        <div class="right">
+          <h3>WEB DEVELOPER/ONLINE TEACHER</h3>
+          <p class="img">
+            <img src="../../assets/img/technology/vue.jpg" width="auto" height="30" alt="">
+          </p>
+          <p class="company">Zzt EDU</p>
+          <p class="content">
+            Responsible for the development of independent official website of some subjects of the company.And also as an online teacher of WEB.Technology consists of HTML5, CSS3, JavaScript, Nodejs, vue, Canvas, Design pattern & Algorithm。
+          </p>
+        </div>
+      </li>
+      <li>
+        <div class="left">
+          <p class="time">2020-</p>
+        </div>
+        <div class="right">
+          <h3>WEB DEVELOPER/ONLINE TEACHER</h3>
+          <p class="img">
+            <img src="../../assets/img/technology/vue.jpg" width="auto" height="30" alt="">
+          </p>
+          <p class="company">Zzt EDU</p>
+          <p class="content">
+            Responsible for the development of independent official website of some subjects of the company.And also as an online teacher of WEB.Technology consists of HTML5, CSS3, JavaScript, Nodejs, vue, Canvas, Design pattern & Algorithm。
+          </p>
+        </div>
+      </li>
+    </ul>
+  </article>
+</template>
+
+<script>
+export default {
+  name: "Work"
+}
+</script>
+
+<style scoped lang="less">
+.work{
+  ul {
+    margin-top: 40px;
+    li{
+      display: flex;
+      div.left{
+        width: 200px;
+        p.time{
+          position: relative;
+          top: -12px;
+          width: 160px;
+          height: 30px;
+          line-height: 30px;
+          text-align: center;
+          font-weight: 700;
+          background-color: #bfe2e6;
+          &::after{
+            position: absolute;
+            top: 0;
+            right: -30px;
+            content: "";
+            width: 0;
+            height: 0;
+            border: 15px solid transparent;
+            border-left-color: #bfe2e6;
+          }
+        }
+      }
+      div.right{
+        box-sizing: border-box;
+        flex: 1;
+        padding-left: 20px;
+        padding-right: 40px;
+        padding-bottom: 40px;
+        border-left: 2px solid #bfe2e6;
+        h3{
+          position: relative;
+          margin-bottom: 20px;
+          margin-top: -10px;
+          line-height: 25px;
+          font-size: 20px;
+          &::before{
+            position: absolute;
+            content: "";
+            top: 10px;
+            left: -23px;
+            width: 4px;
+            height: 4px;
+            background-color: #000;
+          }
+        }
+        p.img{
+          margin-bottom: 20px;
+        }
+        p.company{
+          color: #000;
+          margin-bottom: 20px;
+        }
+        p.content{
+          color: #777;
+          letter-spacing: 1px;
+          font-size: 16px;
+        }
+      }
+      &:last-child{
+        div.right{
+          border-color: transparent;
+        }
+      }
+    }
+  }
+}
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/components/admin/AdminLinkAdd.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="AdminLinkAdd">
+    <h3>添加友链</h3>
+    <el-form :model="form">
+      <el-form-item label="名称">
+        <el-input v-model="form.name"></el-input>
+      </el-form-item>
+      <el-form-item label="链接">
+        <el-input v-model="form.src"></el-input>
+      </el-form-item>
+      <el-form-item label="图标链接">
+        <el-input v-model="form.icon"></el-input>
+      </el-form-item>
+      <el-form-item label="描述">
+        <el-input v-model="form.des"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="handleSubmit"
+        >提交</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminLinkAdd",
+  data(){
+    return {
+      form: {
+        src: "",
+        name: "",
+        icon: "",
+        des: ""
+      }
+    }
+  },
+  methods: {
+    async handleSubmit(){
+      let {data} = await this.$axios({
+        method: "post",
+        url: "/admin/addLink",
+        data: {
+          src: this.form.src,
+          icon: this.form.icon,
+          name: this.form.name,
+          des: this.form.des
+        }
+      })
+
+      if (data.code !== 0)return
+
+      this.$message.success("添加成功")
+      this.$router.push("/link")
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
